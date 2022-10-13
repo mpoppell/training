2d6808dcb27867b3a82a7684093fcd1b2e6b999a
Matthew Poppell <matthew.poppell@gmail.com>
2022-10-13 15:26:01 +1100

    need data lesson l5

---
+++ vuemastery/real-world-vue/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '../views/EventList.vue'
+import AboutView from '../views/AboutView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'EventList',
+    component: EventList,
+  },
+  {
+    path: '/about',
+    name: 'AboutView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: AboutView,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
