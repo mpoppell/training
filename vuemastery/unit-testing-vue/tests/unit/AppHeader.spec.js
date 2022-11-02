b29eabda9b513f685adc7954a4fbe58e71756af1
mpoppell <matthew.poppell@gmail.com>
2022-11-02 16:15:54 +1100

    started real world testing

---
+++ vuemastery/unit-testing-vue/tests/unit/AppHeader.spec.js
@@ -0,0 +1,14 @@
+// import { mount } from '@vue/test-utils'
+// import AppHeader from '@/components/AppHeader'
+
+describe('AppHeader', () => {
+  test('if user is not logged in, do not show logout button', () => {
+    // test body
+    expect(true).toBe(true)
+  })
+
+  test('if a user is logged in, show logout button', () => {
+    // test body
+    expect(true).toBe(true)
+  })
+})
