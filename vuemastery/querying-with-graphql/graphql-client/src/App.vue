74b2b503724dbd3fcea2dc974a85a496f1461573
Matthew Poppell <matthew.poppell@gmail.com>
2022-12-05 16:18:47 +1100

    start L9

---
+++ vuemastery/querying-with-graphql/graphql-client/src/App.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+      <div>
+        <button v-if="!showNewBookForm" @click="showNewBookForm = true">
+          Add a new book
+        </button>
+        <AddBook v-if="showNewBookForm" :search="searchTerm" @closeForm="showNewBookForm = false" />
+      </div>
+      <input type="text" v-model="searchTerm" />
+      <p v-if="loading">Loading...</p>
+      <p v-else-if="error">Something went wrong! Please try again</p>
+      <template v-else>
+        <p v-if="activeBook">
+          Update "{{ activeBook.title }}" rating:
+          <EditRating
+            :initial-rating="activeBook.rating"
+            :book-id="activeBook.id"
+            @closeForm="activeBook = null"
+          />
+        </p>
+        <template v-else>
+          <p v-for="book in books" :key="book.id">
+            {{ book.title }} - {{ book.rating }}
+            <button @click="activeBook = book">Edit rating</button>
+          </p>
+        </template>
+      </template>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue'
+  import { useQuery } from '@vue/apollo-composable'
+  import ALL_BOOKS_QUERY from './graphql/allBooks.query.gql'
+  import EditRating from './components/EditRating.vue'
+  import AddBook from './components/AddBook.vue'
+  import { computed } from '@vue/reactivity'
+
+
+  
+  export default {
+    name: 'App',
+    components: {
+      EditRating,
+      AddBook
+    },
+    setup() {
+      const searchTerm = ref('')
+      const activeBook = ref(null)
+      const showNewBookForm = ref(false)
+  
+      const { result, loading, error } = useQuery(
+        ALL_BOOKS_QUERY,
+        () => ({
+          search: searchTerm.value,
+        }),
+        () => ({
+          debounce: 500,
+          // enabled: searchTerm.value.length > 2
+        })
+      )
+  
+      const books = computed(() => result.value?.allBooks ?? [])
+  
+      return { books, searchTerm, loading, error, activeBook, showNewBookForm }
+    },
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
+  .list-wrapper {
+    display: flex;
+    margin: 0 auto;
+    max-width: 960px;
+  }
+  
+  .list {
+    width: 50%;
+  }
+  </style>
---
+++ vuemastery/querying-with-graphql/graphql-client/src/main.js
@@ -0,0 +1,29 @@
+import { createApp, h, provide } from 'vue'
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+import App from './App.vue'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App),
+})
+
+app.mount('#app')
