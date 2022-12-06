13756490ad1be75226eaed9a91a9af0ee33c4245
Matthew Poppell <matthew.poppell@gmail.com>
2022-12-06 16:22:19 +1100

    end 10 not working

--- vuemastery/querying-with-graphql/graphql-client/src/App.vue
+++ vuemastery/querying-with-graphql/graphql-client/src/App.vue
@@ -1,89 +1,131 @@
 <template>
     <div>
-      <div>
-        <button v-if="!showNewBookForm" @click="showNewBookForm = true">
-          Add a new book
-        </button>
-        <AddBook v-if="showNewBookForm" :search="searchTerm" @closeForm="showNewBookForm = false" />
-      </div>
-      <input type="text" v-model="searchTerm" />
-      <p v-if="loading">Loading...</p>
-      <p v-else-if="error">Something went wrong! Please try again</p>
-      <template v-else>
-        <p v-if="activeBook">
-          Update "{{ activeBook.title }}" rating:
-          <EditRating
-            :initial-rating="activeBook.rating"
-            :book-id="activeBook.id"
-            @closeForm="activeBook = null"
-          />
-        </p>
+        <div>
+            <button
+                v-if="!showNewBookForm"
+                @click="showNewBookForm = true"
+            >
+                Add a new book
+            </button>
+            <AddBook
+                v-if="showNewBookForm"
+                :search="searchTerm"
+                @closeForm="showNewBookForm = false"
+            />
+        </div>
+        <input
+            type="text"
+            v-model="searchTerm"
+        />
+        <p v-if="loading">Loading...</p>
+        <p v-else-if="error">Something went wrong! Please try again</p>
         <template v-else>
-          <p v-for="book in books" :key="book.id">
-            {{ book.title }} - {{ book.rating }}
-            <button @click="activeBook = book">Edit rating</button>
-          </p>
+            <p v-if="activeBook">
+                Update "{{ activeBook.title }}" rating:
+                <EditRating
+                    :initial-rating="activeBook.rating"
+                    :book-id="activeBook.id"
+                    @closeForm="activeBook = null"
+                />
+            </p>
+            <template v-else>
+                <section class="list-wrapper">
+                  <div class="list">
+                    <h3>All Books</h3>
+                    <p v-for="book in books" :key="book.id">
+                      {{ book.title }} - {{ book.rating }}
+                      <button @click="activeBook = book">Edit rating</button>
+                    </p>
+                  </div>
+                  <div class="list">
+                    <h3>Favorite Books</h3>
+                    <p v-for="book in favBooksResult.favoriteBooks" :key="book.id">
+                      {{ book.title }}
+                    </p>
+                  </div>
+                </section>
+              </template>
         </template>
-      </template>
     </div>
-  </template>
+</template>
   
-  <script>
-  import { ref } from 'vue'
-  import { useQuery } from '@vue/apollo-composable'
-  import ALL_BOOKS_QUERY from './graphql/allBooks.query.gql'
-  import EditRating from './components/EditRating.vue'
-  import AddBook from './components/AddBook.vue'
-  import { computed } from '@vue/reactivity'
+<script>
+import { ref } from 'vue'
+import { useQuery } from '@vue/apollo-composable'
+import ALL_BOOKS_QUERY from './graphql/allBooks.query.gql'
+import BOOK_SUBSCRIPTION from './graphql/newBook.subscription.gql'
+import FAVORITE_BOOKS_QUERY from './graphql/favoriteBooks.query.gql'
+
+import EditRating from './components/EditRating.vue'
+import AddBook from './components/AddBook.vue'
+import { computed } from '@vue/reactivity'
 
 
-  
-  export default {
+
+export default {
     name: 'App',
     components: {
-      EditRating,
-      AddBook
+        EditRating,
+        AddBook
     },
     setup() {
-      const searchTerm = ref('')
-      const activeBook = ref(null)
-      const showNewBookForm = ref(false)
-  
-      const { result, loading, error } = useQuery(
-        ALL_BOOKS_QUERY,
-        () => ({
-          search: searchTerm.value,
-        }),
-        () => ({
-          debounce: 500,
-          // enabled: searchTerm.value.length > 2
-        })
-      )
-  
-      const books = computed(() => result.value?.allBooks ?? [])
-  
-      return { books, searchTerm, loading, error, activeBook, showNewBookForm }
+        const searchTerm = ref('')
+        const activeBook = ref(null)
+        const showNewBookForm = ref(false)
+
+        const { result, loading, error, subscribeToMore } = useQuery(
+            ALL_BOOKS_QUERY,
+            () => ({
+                search: searchTerm.value,
+            }),
+            () => ({
+                debounce: 500,
+                // enabled: searchTerm.value.length > 2
+            })
+        )
+        subscribeToMore(() => ({
+            document: BOOK_SUBSCRIPTION,
+            updateQuery(previousResult, newResult) {
+                const res = {
+                    allBooks: [
+                        ...previousResult.allBooks,
+                        newResult.subscriptionData.data.bookSub,
+                    ],
+                }
+                return res
+            },
+        }))
+
+        const books = computed(() => result.value?.allBooks ?? [])
+
+        const { result: favBooksResult } = useQuery(
+            FAVORITE_BOOKS_QUERY
+            )
+        console.dir(favBooksResult.value)
+
+        return { books, searchTerm, loading, error, activeBook, showNewBookForm }
     },
-  }
-  </script>
+}
+</script>
   
-  <style>
-  #app {
+<style>
+#app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
-  }
-  
-  .list-wrapper {
+}
+
+.list-wrapper {
     display: flex;
     margin: 0 auto;
     max-width: 960px;
-  }
-  
-  .list {
+}
+
+.list {
     width: 50%;
-  }
-  </style>
+}
+
+</style>
--- vuemastery/querying-with-graphql/graphql-client/src/main.js
+++ vuemastery/querying-with-graphql/graphql-client/src/main.js
@@ -1,29 +1,71 @@
 import { createApp, h, provide } from 'vue'
 import {
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache,
+    split,
 } from '@apollo/client/core'
 import { DefaultApolloClient } from '@vue/apollo-composable'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+import typeDefs from './graphql/typedefs.gql'
+import FAVORITE_BOOKS_QUERY from './graphql/favoriteBooks.query.gql'
 
 import App from './App.vue'
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000/graphql`,
+    options: {
+        reconnect: true,
+    },
 })
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/graphql',
+})
+
+
+const link = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query)
+        return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+        )
+    },
+    wsLink,
+    httpLink
+)
+
 
 const cache = new InMemoryCache()
 
+cache.writeQuery({
+    query: FAVORITE_BOOKS_QUERY,
+    
+        data: {
+            favoriteBooks: [
+              {
+                title: 'My Book',
+                id: 123,
+                rating: 5,
+              },
+            ],
+       
+    },
+  })
+
 const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
+    link,
+    cache,
+    typeDefs,
 })
 
 const app = createApp({
-  setup() {
-    provide(DefaultApolloClient, apolloClient)
-  },
-  render: () => h(App),
+    setup() {
+        provide(DefaultApolloClient, apolloClient)
+    },
+    render: () => h(App),
 })
 
 app.mount('#app')
