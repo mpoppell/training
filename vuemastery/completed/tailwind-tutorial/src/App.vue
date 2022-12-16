0faa8f7910bd57c92cdc067ab54abb4e6fa48163
Matthew Poppell <matthew.poppell@gmail.com>
2022-12-16 16:46:14 +1100

    not a fan, finished at L5

--- vuemastery/completed/tailwind-tutorial/src/App.vue
+++ vuemastery/completed/tailwind-tutorial/src/App.vue
@@ -11,9 +11,9 @@ function updateCart(id) {
 </script>
 
 <template>
-  <div class="nav-bar"></div>
+  <div class="h-[60px] mb-[25px] bg-gradient-to-r from-[#16c0b0] to-[#84cf6a]"></div>
 
-  <div class="cart">Cart({{ cart.length }})</div>
+  <div class="my-[25px] mc-[100px] p-[30px] border border-solid border-[]">Cart({{ cart.length }})</div>
   <product-display :premium="premium" @add-to-cart="updateCart"></product-display>
 </template>
 
